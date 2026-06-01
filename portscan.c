73e9336b07932a62f585c6ec65e847c1e448e3fc
corujareal <thiagorafaelportellasilva@gmail.com>
2026-06-01 00:45:39 +0000

    Update portscan.c

--- portscan.c
+++ portscan.c
@@ -1,34 +1,47 @@
 #include <stdio.h>
 #include <sys/socket.h>
-#include <netdb.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h> 
+
 int main(int argc, char *argv[]) {
-	if (argc < 2) {
-		printf("Uso: %s <IP>\n", argv[0]);
-		return 1;
-	}
-	int sock;
-	int conecta;
-	int porta;
-	int inicio = 1;
-	int final = 1024;   // só portas baixas para não demorar
-	char *destino;
-	destino = argv[1];
-	struct sockaddr_in alvo;
-	for (porta = inicio; porta <= final; porta++) {
-		sock = socket(AF_INET, SOCK_STREAM, 0);
-		if (sock < 0) {
-			perror("socket");
-			continue;
-		}
-		alvo.sin_family = AF_INET;
-		alvo.sin_port = htons(porta);
-		alvo.sin_addr.s_addr = inet_addr(destino);
-		conecta = connect(sock, (struct sockaddr *)&alvo, sizeof alvo);
-		if (conecta == 0) {
-			printf("Porta aberta: %d\n", porta);
-		}
-		pclose(sock);
-	}
-	return 0;
+    if (argc < 2) {
+        printf("Uso: %s <IP>\n", argv[0]);
+        return 1;
+    }
+
+    int sock;
+    int conecta;
+    int porta;
+    int inicio = 1;
+    int final = 1024;
+    char *destino = argv[1];
+    struct sockaddr_in alvo;
+
+    // Converte o endereço IP uma única vez (fora do loop)
+    alvo.sin_family = AF_INET;
+    alvo.sin_addr.s_addr = inet_addr(destino);
+    if (alvo.sin_addr.s_addr == INADDR_NONE) {
+        printf("Erro: IP inválido: %s\n", destino);
+        return 1;
+    }
+
+    for (porta = inicio; porta <= final; porta++) {
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (sock < 0) {
+            perror("socket");
+            continue;
+        }
+
+        alvo.sin_port = htons(porta);
+
+        conecta = connect(sock, (struct sockaddr *)&alvo, sizeof(alvo));
+        if (conecta == 0) {
+            printf("Porta aberta: %d\n", porta);
+        }
+
+        close(sock);   // corrigido: era pclose()
+    }
+
+    return 0;
 }
